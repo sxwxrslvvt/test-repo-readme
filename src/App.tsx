c90b4ac1c3b3bb6573df134475048190b4276a27
sxwxrslvvt <156970719+sxwxrslvvt@users.noreply.github.com>
2025-10-28 15:12:34 +0300

    Удален src/App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { TimelineBlock } from './src/components/TimelineBlock';
-import './src/styles/main.scss';
-
-export const App = () => (
-  <div>
-    <TimelineBlock />
-  </div>
-);
